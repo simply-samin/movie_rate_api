7f2f4f3803be5828fb4cc9f2cec6e4605c0319a9
samin_nextgenit <samiulislamsamin@nextgenitltd.com>
2024-02-23 01:57:28 +0600

    chroe: remove unused imports

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\RedirectResponse;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
@@ -17,9 +14,8 @@ class VerifyEmailController extends Controller
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request)
+    public function __invoke(EmailVerificationRequest $request): JsonResponse
     {
-        // Retrieve user using id
         $user = $request->user();
 
         // Check if email is already verified
